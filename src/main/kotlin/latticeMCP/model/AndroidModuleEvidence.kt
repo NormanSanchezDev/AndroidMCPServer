bfc20515b596f3c717203bdb2bfce22e4872d71e
Norman Sanchez <hello@normansanchez.dev>
2026-08-26 02:28:19 -0600

    ✨Start MCP Android project.

--- src/main/kotlin/latticeMCP/model/AndroidModuleEvidence.kt
+++
@@ -1,9 +0,0 @@
-package dev.lattice.androidmcp.latticeMCP.model
-
-data class AndroidModuleEvidence(
-    val name: String,
-    val path: String,
-    val type: AndroidModuleType,
-    val buildFile: String,
-    val manifest: String?
-)
